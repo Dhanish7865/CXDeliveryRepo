ec1e2e0949f8db02d6ff4720a911a49185a30485
Dhanish <dhakhtar@cisco.com>
2020-08-25 05:38:02 -0400

    Commit Changes to index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Web Form</h1>
+
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname"><br>
+  <label for="age">Age:</label></label><br>
+  <input type="text" id="age" name="age"><br>
+  <label for="location">Location:</label><br>
+  <input type="text" id="location" name="location"><br>
+  <label for="shoe size">Shoe Size:</label><br>
+  <input type="text" id="shoe size" name="shoe size"><br>
+  <input type="submit" value="Submit">
+
+</form>
+
+</body>
+</html
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~                                                                                                                  
+~        
